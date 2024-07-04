8c9d33ec238de3816fc3570880938e29a2d8b058
GevaX <gevax1010@gmail.com>
2024-07-04 20:53:03 +0300

    Created add task menu

--- lib/widgets/add_task_button.dart
+++ lib/widgets/add_task_button.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
 
+import 'add_task_menu.dart';
+
 class AddTaskButton extends StatelessWidget {
   const AddTaskButton({super.key});
 
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      onPressed: () {
-        print('object');
-      },
       backgroundColor: Theme.of(context).primaryColor,
       foregroundColor: Colors.black,
       child: const Icon(Icons.add),
+      onPressed: () => _showAddTaskMenu(context)
+    );
+  }
+  void _showAddTaskMenu(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      enableDrag: false,
+      builder: (BuildContext context) {
+        return const AddTaskMenu(); // Returns your BottomSheet widget
+      },
     );
   }
 }
---
+++ lib/widgets/add_task_menu.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AddTaskMenu extends StatelessWidget {
+  const AddTaskMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 250,
+    );
+  }
+}
